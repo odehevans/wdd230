8b90681e3eb9c18f977ca76850f061332c9162e9
Mapopo (Evans) <mppdgreat@gmail.com>
2023-01-27 19:02:22 +0100

    hidden input date

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -1,2 +1,4 @@
 document.querySelector("#current-year").textContent = `${new Date().getFullYear()}`;
 document.querySelector("#last-modified").textContent = `Last Modification: ${document.lastModified}`;
+hiddenInput = document.querySelector("#current-hidden-date");
+if (hiddenInput) hiddenInput.value = `${new Date().toLocaleString()}`;
