5d5e0bba17b0985496308b8b377cac5a3b61def4
Mapopo (Evans) <mppdgreat@gmail.com>
2023-02-01 21:09:21 +0100

    Weather API activity

---
+++ week05/scripts/weather.js
@@ -0,0 +1,30 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Trier&units=imperial&appid=99fedeb9ab4dbde1f00c1d5fe29d45d0';
+
+async function apiFetch(){
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      displayResults(data);
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt',`${desc}`);
+  captionDesc.textContent = `${desc}`;
+}
+
+apiFetch();
