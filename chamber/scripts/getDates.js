5c76c7500c5d432411d001f7df8fe2bbbe3e6c1c
Mapopo (Evans) <mppdgreat@gmail.com>
2023-02-16 00:21:47 +0100

    show banner

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -1,4 +1,13 @@
 document.querySelector("#current-year").textContent = `${new Date().getFullYear()}`;
 document.querySelector("#last-modified").textContent = `Last Modification: ${document.lastModified}`;
 hiddenInput = document.querySelector("#current-hidden-date");
-if (hiddenInput) hiddenInput.value = `${new Date().toLocaleString()}`;
+const currentDate = new Date();
+if (hiddenInput) hiddenInput.value = `${currentDate.toLocaleString()}`;
+
+const weekday = currentDate.getDay();
+if ([1,2,3].includes(weekday)) {
+  const banner = document.createElement('div');
+  banner.innerHTML = "<em>Your are invited to attend a chamber of commerce meet and greet on Wednesday at 7:00 pm.</em>";
+  banner.id = "page-banner";
+  document.querySelector("footer").append(banner);
+}
